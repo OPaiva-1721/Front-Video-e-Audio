ad401d4b7af3e1a9651027d49680765bf773d42b
OPaiva-1721 <gabryelpaiva123@gmail.com>
2025-05-28 21:48:59 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://video-e-audio.onrender.com/api/download';
+const API_URL = process.env.REACT_APP_API_URL || 'https://video-e-audio.onrender.com/api';
 
 interface DownloadRequest {
   url: string;
